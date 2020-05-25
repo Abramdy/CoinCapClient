93ac6fc8c0056815474f79ecf7d11f0af6e3fb94
Oleg Bech <37021780+Bechol@users.noreply.github.com>
2020-05-25 11:35:26 +0300

    Fix greeting controller

--- src/main/java/ru/bechol/CoinCapClient/controllers/GreetingController.java
+++ src/main/java/ru/bechol/CoinCapClient/controllers/GreetingController.java
@@ -1,10 +1,11 @@
 package ru.bechol.CoinCapClient.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
+    @GetMapping
     public String getGreetingView() {
         return "index";
     }
